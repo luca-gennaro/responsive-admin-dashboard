c0200b439a90f0557d4a008aba3474cd245a298c
Luca Gennaro <lucagennaro91@gmail.com>
2023-02-12 15:07:53 +0100

    Filling Table with Orders using JavaScript

---
+++ orders.js
@@ -0,0 +1,45 @@
+const Orders = [
+    {
+        productName: "GPS 4k Drone",
+        productNumber: "22453",
+        paymentStatus: "Paid",
+        shipping: "Pending"
+    },
+    {
+        productName: "DJI Air 25",
+        productNumber: "66547",
+        paymentStatus: "Paid",
+        shipping: "Shipped"
+    },
+    {
+        productName: "Lozenge Drone",
+        productNumber: "33790",
+        paymentStatus: "Due",
+        shipping: "Declined"
+    },
+    {
+        productName: "Foldable Mini Drone",
+        productNumber: "63467",
+        paymentStatus: "Due",
+        shipping: "Pending"
+    },
+    {
+        productName: "Drone with Camera",
+        productNumber: "02934",
+        paymentStatus: "Paid",
+        shipping: "Shipped"
+    },
+    {
+        productName: "Luke Space Drone",
+        productNumber: "66543",
+        paymentStatus: "Paid",
+        shipping: "Shipped"
+    },
+    {
+        productName: "GAir Drone",
+        productNumber: "98765",
+        paymentStatus: "Due",
+        shipping: "Shipped"
+    },
+    
+]
